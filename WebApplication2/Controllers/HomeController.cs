3788e36c5d6de162e37ed8dca9197187eb4df7ad
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2016-07-06 22:19:33 +0400

    AJAX.BeginForm example with Viewbag
    MVC App meets ajax

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home  
+        [HttpGet]
+        public ActionResult EmployeeMaster()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EmployeeMaster(EmpModel obj)
+        {
+            ViewBag.Records = "Name : " + obj.Name + " City:  " + obj.City + " Address: " + obj.Address;
+            return PartialView("EmployeeMaster");
+        }  
+    }
+}
---
+++ WebApplication2/Models/EmpModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class EmpModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Address { get; set; }  
+    }
+}
